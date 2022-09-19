f7732234e12dc281a4709a990d7de55652884c18
william5 <samwilliamj333@gmail.com>
2022-09-19 16:58:33 +0530

    js vowels revnum revnuminword sumofdigit

--- index.js
+++ index.js
@@ -75,3 +75,20 @@ while(revnum>0){
     revnum=(`number in rev order`,revnum-rem)/10;
 }
 
+//find vowels
+function vowelcount(oneword){
+    var count=0;
+    var vowels='aeiou';
+    for(let i=0;i<oneword.length;i++){
+        if(vowels.indexOf(oneword[i]) > -1){
+            count++;
+        }
+    }
+    return +count
+}
+
+var totalvowels=vowelcount('saasaeiofsdfdsfsfoos')
+console.log(`Total Vowels:`,totalvowels);
+
+
+
